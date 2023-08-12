27d22e6bc8a1c452e8219558b8717d62c5a4aa6e
jovanovskajovana <jovanovska.jovana1@gmail.com>
2023-08-12 15:22:04 +0200

    Fix helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,7 +4,7 @@ import { supportedCurrencies } from '../constants/data'
  * Map supported currency to match Ticker pairs.
  */
 export const getSupportedCurrencyPairs = (currency: string) =>
-  supportedCurrencies.map((item) => `${currency}${item}`)
+  supportedCurrencies.map((item) => `${currency}${item.id}`)
 
 /**
  * Format number to comma-separated digits.
